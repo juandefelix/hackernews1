38d8867248760ad233879851646ee0c221436297
Apprentice <apprentice@dbc-chicago-015-109.local>
2014-02-08 20:18:30 -0600

    Votes working

--- public/js/application.js
+++ public/js/application.js
@@ -1,41 +1,24 @@
-
-
-// function counter = {
-//    this.up = $(".up");
-//    this.down = $(".down")}
-
-// $(document).ready(function() {
-
-//   $(".up").click(function(){
-
-//   });
-//   $(.down).click(function(){
-
-//   });
-// });
-
+// We are trying to update number of votes when we click a button
 
 $(document).ready(function(){
 
-  // var up = $(".up")
-
+// we are listening for any 'form' to be clicked
+//'e' is the event created when we click a button
   $("form").submit(function(e){
     e.preventDefault();
-    $.post( "/up",  $("form").serialize(), function(data){
-      console.log(e.target);
+
+    // 'var = target' is the hidden data that we stored on the form. (See 'index.erb')
+    // The hidden data is the post id we want to update.
+    var target = $(e.target).data("id");
+
+    // We send the id of the post to our '/up' method in the backend.
+    // The '/up' method will update the votes and return the value of the votes (data)
+    $.post( "/up",  {'id': target}, function(data){
+
+      // Then we select the element in the html where we display the votes
+      // (it will have an id='vote<%=vote.id%>'),
+      // and change the text with the new value ('data')
+      $('#vote' + target).text("votes: " + data);
     });
-
-
   });
-
-
-
-  // $.ajax({
-  //   type: "POST",
-  //   url : '/up',
-  //   data : e.serialize();
-  //   success : function(result){
-  //     console.log("hola");}
-  //  });
-
 });
